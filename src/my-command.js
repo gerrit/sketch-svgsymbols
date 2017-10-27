907f456542bf6728314924d7dba8e3d94ffd8718
Gerrit Kaiser <gerrit@gerritkaiser.de>
2017-10-27 18:53:20 +0200

    ability to parse and filter SVG export

--- src/my-command.js
+++ src/my-command.js
@@ -1,9 +1,40 @@
-export function svgSymbolsHandler (context) {
+import XML2JS from 'xml2js'
+
+function showMessage(str) {
+  // HACK because `context` in an action handler doesn't have a `document`
   const windows = NSApp.windows()
   for(let i = 0; i< windows.length; i++) {
     if (windows[i].isKindOfClass(MSDocumentWindow)) {
       const doc = windows[i].document()
-      doc.showMessage("It's alive 🙌")
+      doc.showMessage(str)
     }
   }
+}
+
+function filterSVGFile(path) {
+  var svgString = "" + NSString.stringWithContentsOfFile_encoding_error(path, NSUTF8StringEncoding, nil)
+  log(svgString)
+  // svgString = 'munged'
+  var builder = new XML2JS.Builder()
+  XML2JS.parseString(svgString, function(err, parsed) {
+    console.log(JSON.stringify(parsed))
+    parsed['svg']['desc'] = 'Created with sketch-svgsymbols'
+    var transformedSvgString = builder.buildObject(parsed)
+    console.log(transformedSvgString)
+    NSString.stringWithString(transformedSvgString).writeToFile_atomically_encoding_error(path, true, NSUTF8StringEncoding, nil)
+  })
+}
+
+export function svgSymbolsHandler(context, params) {
+  var exports = context.actionContext.exports
+  var filesToCompress = []
+  for (var i = 0; i < exports.count(); i++) {
+    var currentExport = exports.objectAtIndex(i)
+    log('export')
+    log(currentExport)
+    if (currentExport.request.format() == 'svg') {
+      filterSVGFile(currentExport.path)
+    }
+  }
+  showMessage("It's alive 🙌")
 }
